77b23a10b58efbd30d1302cfcba293f9b3b9bdfb
shannon horn <shannonahorn@icloud.com>
2021-07-22 00:52:58 -0700

    packages updates (#6)

--- components/packages.js
+++ components/packages.js
@@ -6,12 +6,34 @@ const Packages = ({ packages }) => {
       <a id="packages-and-pricing"></a>
       <div className="copy-container flow">
         <h2>PACKAGES</h2>
-        <section className={`${styles.packages_container} flow`}>
+        <section className={styles.packages_container}>
           {packages.map((currentPackage) => (
             <article key={currentPackage.id} className={styles.package}>
-              <div className={styles.package_name}>{currentPackage.name}</div>
-              <div className={`${styles.package_body} flow`}>
-                {currentPackage.description}
+              <div
+                className={`${styles.package_name} ${
+                  currentPackage.name.includes("Premium")
+                    ? styles.package_premium
+                    : ""
+                }`}
+              >
+                {currentPackage.name}
+              </div>
+              <div className={`${styles.package_body_container} flow`}>
+                <div className={styles.package_body_title}>What's Included</div>
+                <div className={styles.package_body}>
+                  {currentPackage.description}
+                </div>
+                <h3 className={`price ${styles.package_price}`}>
+                  ${currentPackage.price}
+                </h3>
+                <a
+                  id={currentPackage.id}
+                  className={`btn btn-primary ${styles.btn_select_package}`}
+                  data-package={currentPackage.id}
+                  href="#"
+                >
+                  Select This Package
+                </a>
               </div>
             </article>
           ))}
