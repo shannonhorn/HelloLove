f336f75d90161ff9eab4100a086d514cdbd0200b
shannon horn <shannonahorn@icloud.com>
2021-08-24 06:54:09 +0000

    Added header info and plausible script.

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,18 @@ const Home = (props) => {
     <>
       <Head>
         <title>Hello Love Ultrasound: Welcome Home</title>
+        <script
+          defer
+          data-domain="hellolove.life"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+        <meta
+          name="description"
+          content="Hello Love offers 3D 4D 5D ultrasounds in Peoria Arizona. We are a full service ultrasound clinic providing superior images of your baby and expert customer service. Call today to schedule your appointment! (623) 556-6767"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <a id="top"></a>
